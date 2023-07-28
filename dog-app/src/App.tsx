2884c1fd3ee4ef648408e38fac3b132123bcc467
masu <yasumoto.7991mm@gmail.com>
2023-07-28 11:15:10 +0700

    create dog button

--- dog-app/src/App.tsx
+++ dog-app/src/App.tsx
@@ -8,13 +8,18 @@ type DogResponse = {
 function App() {
   const [dogImage, setDogImage] = useState<string | null>(null);
 
-  useEffect(() => {
+  // APIから犬の画像を取得する関数
+  const fetchDogImage = () => {
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((response) => response.json())
       .then((data: DogResponse) => setDogImage(data.message))
       .catch((error) => console.log("Error:", error));
-  }, []);
+  };
 
+  // コンポーネントがマウントされたときに初めて画像を取得
+  useEffect(fetchDogImage, []);
+
+  // 画像がまだ読み込まれていない場合
   if (dogImage === null) {
     return <div>Loading...</div>;
   }
@@ -23,6 +28,7 @@ function App() {
     <div>
       <h1>Random Dog Image</h1>
       <img src={dogImage} alt="A random dog" />
+      <button onClick={fetchDogImage}>Dog</button>
     </div>
   );
 }
