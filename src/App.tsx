b29893ef96b39c0550521972379f376856cbf620
masu <yasumoto.7991mm@gmail.com>
2023-08-09 10:59:01 +0800

    redirect in default view

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import GetNewDog from "./components/get-new-dog/GetNewDog";
 import SignInWithGoogle from "./components/sign-in/SignInWithGoogle";
 
@@ -6,6 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/get-new-dog" element={<GetNewDog />}></Route>
         <Route path="/login" element={<SignInWithGoogle />}></Route>
       </Routes>
