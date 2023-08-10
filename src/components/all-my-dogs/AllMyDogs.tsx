ba4abc8e29e087fbc32c3000ed7faae25e843d5d
masu <yasumoto.7991mm@gmail.com>
2023-08-10 10:53:15 +0800

    Fixed to show my-dogs when reloading

--- src/components/all-my-dogs/AllMyDogs.tsx
+++ src/components/all-my-dogs/AllMyDogs.tsx
@@ -1,40 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { auth, db } from "../../firebase";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 function AllMyDogs() {
   const [dogImages, setDogImages] = useState<string[]>([]);
 
   useEffect(() => {
-    // 現在のユーザーIDを取得
-    const currentUserId = auth.currentUser?.uid;
+    // 認証状態の変化を監視
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // ユーザーが認証されている場合のみデータを取得
+        const userDogImagesRef = collection(db, "users", user.uid, "dogImages");
 
-    if (currentUserId) {
-      // Firestoreからユーザーの犬の画像を取得
-      const userDogImagesRef = collection(
-        db,
-        "users",
-        currentUserId,
-        "dogImages"
-      );
-
-      getDocs(userDogImagesRef)
-        .then((snapshot) => {
-          const fetchedImages: string[] = [];
-          snapshot.forEach((doc) => {
-            const data = doc.data();
-            fetchedImages.push(data.url);
-            console.log(data);
+        getDocs(userDogImagesRef)
+          .then((snapshot) => {
+            const fetchedImages: string[] = [];
+            snapshot.forEach((doc) => {
+              const data = doc.data();
+              fetchedImages.push(data.url);
+            });
+            setDogImages(fetchedImages);
+          })
+          .catch((error) => {
+            console.error("Error fetching dog images:", error.message);
           });
-          setDogImages(fetchedImages);
-        })
-        .catch((error) => {
-          console.error("Error fetching dog images:", error.message);
-        });
-    }
-  }, []);
+      }
+    });
 
-  console.log(dogImages);
+    // コンポーネントのクリーンアップ時にリスナーを解除
+    return () => unsubscribe();
+  }, []);
 
   return (
     <div>
