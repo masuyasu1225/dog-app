7dfd84d400cfc53b30b427e82bf9ebad109068d1
masu <yasumoto.7991mm@gmail.com>
2023-08-18 14:58:14 +0800

    Change time and feed even if user opens other tab

--- src/components/get-new-dog/GetNewDog.tsx
+++ src/components/get-new-dog/GetNewDog.tsx
@@ -20,28 +20,16 @@ const MAX_TIMER = 10;
 const GetNewDog: React.FC = () => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [fadeIn, setFadeIn] = useState<boolean>(false);
-
-  // Calculate elapsed time
   const [lastUpdateTime, setLastUpdateTime] = useState<number>(
     Number(localStorage.getItem("lastUpdateTime") || Date.now())
   );
-  const elapsedSeconds = calculateElapsedTimeInSeconds(lastUpdateTime);
-
-  // Get initial feed and timer values
-  const initialFeed = Number(localStorage.getItem("feed") || MAX_FEED);
-  const initialTimer = Number(localStorage.getItem("timer") || MAX_TIMER);
-
-  const { updatedFeed, updatedTimer } = updateFeedAndTimer(
-    initialFeed,
-    initialTimer,
-    elapsedSeconds,
-    MAX_FEED,
-    MAX_TIMER
-  );
-
   const [dogImage, setDogImage] = useState<string | null>(doghouse);
-  const [feed, setFeed] = useState<number>(updatedFeed);
-  const [timer, setTimer] = useState<number>(updatedTimer);
+  const [feed, setFeed] = useState<number>(
+    Number(localStorage.getItem("feed") || MAX_FEED)
+  );
+  const [timer, setTimer] = useState<number>(
+    Number(localStorage.getItem("timer") || MAX_TIMER)
+  );
 
   const saveToFirestore = (imageUrl: string) => {
     if (currentUser) {
@@ -57,14 +45,13 @@ const GetNewDog: React.FC = () => {
 
   const fetchDogImage = () => {
     setFadeIn(false);
-
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((response) => response.json())
       .then((data: DogResponse) => {
         setDogImage(data.message);
         setFadeIn(true);
         if (feed > 0) {
-          setFeed((prevFeed) => prevFeed - 1);
+          setFeed(feed - 1);
         }
         saveToFirestore(data.message);
       })
@@ -79,10 +66,35 @@ const GetNewDog: React.FC = () => {
   }, []);
 
   useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === "visible") {
+        const currentTimestamp = Date.now();
+        const elapsedSeconds = calculateElapsedTimeInSeconds(lastUpdateTime);
+
+        const { updatedFeed, updatedTimer } = updateFeedAndTimer(
+          feed,
+          timer,
+          elapsedSeconds,
+          MAX_FEED,
+          MAX_TIMER
+        );
+
+        setFeed(updatedFeed);
+        setTimer(updatedTimer);
+        setLastUpdateTime(currentTimestamp);
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, [feed, timer, lastUpdateTime]);
+
+  useEffect(() => {
     const handleBeforeUnload = () => {
       localStorage.setItem("lastUpdateTime", String(Date.now()));
     };
-
     window.addEventListener("beforeunload", handleBeforeUnload);
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
@@ -92,7 +104,7 @@ const GetNewDog: React.FC = () => {
   useEffect(() => {
     setLastUpdateTime(Date.now());
     localStorage.setItem("feed", String(feed));
-    localStorage.setItem("timer", String(Math.floor(timer)));
+    localStorage.setItem("timer", String(timer));
   }, [feed, timer]);
 
   useEffect(() => {
@@ -104,7 +116,6 @@ const GetNewDog: React.FC = () => {
         }
       }
     }, 1000);
-
     return () => {
       clearInterval(timerDecreaseId);
     };
